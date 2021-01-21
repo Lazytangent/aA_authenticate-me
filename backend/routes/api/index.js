c794e198e0802d003a6558831cbaa9c7198913bb
Lazytangent <pm123633@gmail.com>
2021-01-20 20:45:18 -0600

    More cleanup

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
-const asyncHandler = require('express-async-handler');
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
 
 router.use('/session', sessionRouter);
 
