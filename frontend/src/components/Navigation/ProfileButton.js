0b72e8fba4bcbe6e6b461660cb5e899948a80322
Lazytangent <pm123633@gmail.com>
2021-01-21 17:12:33 -0600

    Working Nav bar

---
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -0,0 +1,7 @@
+const ProfileButton = () => {
+  return (
+    <i className="fas fa-user"></i>
+  );
+};
+
+export default ProfileButton;
---
+++ frontend/src/components/Navigation/index.js
@@ -0,0 +1,32 @@
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+
+import ProfileButton from './ProfileButton';
+import './Navigation.css';
+
+const Navigation = ({ isLoaded }) => {
+  const sessionUser = useSelector((state) => state.session.user);
+
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = <ProfileButton user={sessionUser} />;
+  } else {
+    sessionLinks = (
+      <>
+        <NavLink to="/login">Log In</NavLink>
+        <NavLink to="/signup">Sign Up</NavLink>
+      </>
+    );
+  }
+
+  return (
+    <ul className="nav-link">
+      <li>
+        <NavLink to="/" exact>Home</NavLink>
+        {isLoaded && sessionLinks}
+      </li>
+    </ul>
+  )
+};
+
+export default Navigation;
