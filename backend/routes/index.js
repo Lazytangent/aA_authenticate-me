1213b921e07347c657e72ddf3d1c2d792b57901d
Lazytangent <pm123633@gmail.com>
2021-01-21 13:16:25 -0600

    Started creating backend root route for production

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,4 +4,14 @@ const router = express.Router();
 const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.sendFile(
+
+    )
+  })
+}
+
 module.exports = router;
